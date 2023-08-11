613370c38f68d0c35e7fcad8a0ff43ae790c26c7
kallemarc <46725076+kallemarc@users.noreply.github.com>
2023-08-12 00:53:44 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -13,57 +13,66 @@ report_ip_list() {
          "https://ipv64.net/api"
 }
 
+# Remove expired IPs from REPORTED_IPS_FILE
+CURRENT_TIMESTAMP=$(date +%s)
+TEMP_REPORTED_IPS_FILE="/var/log/reported_ips_temp.txt"
+touch "$TEMP_REPORTED_IPS_FILE"  # Create the temporary file if it doesn't exist
+while read -r line; do
+    IP=$(echo "$line" | cut -d ' ' -f 1)
+    EXPIRATION_TIME=$(echo "$line" | cut -d ' ' -f 2)
+    if [ "$CURRENT_TIMESTAMP" -le "$EXPIRATION_TIME" ]; then
+        echo "$line" >> "$TEMP_REPORTED_IPS_FILE"
+    fi
+done < "$REPORTED_IPS_FILE"
+mv "$TEMP_REPORTED_IPS_FILE" "$REPORTED_IPS_FILE"
+
 # Extract IPs from the last hour of the SSH log file and create JSON data
 extract_ips_from_ssh_log() {
     local LOG_FILE="$1"
-    local TEMP_REPORTED_IPS_FILE="/tmp/reported_ips.txt"
+    local SUSPICIOUS_IPS=()
     local TEMP_IP_LIST=()
 
     # Calculate the timestamp for one hour ago
-    ONE_HOUR_AGO=$(date -d '1 hour ago' +'%b %d %H:%M:%S')
+    ONE_HOUR_AGO=$(date -d '1 hour ago' +'%Y-%m-%dT%H:%M:%S')
 
-    # Use awk to extract suspicious entries from the log file and extract IPs
-    awk -v one_hour_ago="$ONE_HOUR_AGO" '
-        $1" "$2 >= one_hour_ago && (/Failed password/ || /Failed password for invalid user/) {
-            match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/, ip)
-            if (ip[0] != "") {
-                print ip[0]
-            }
-        }
-    ' "$LOG_FILE" | sort | uniq > "$TEMP_REPORTED_IPS_FILE"
+    # Use awk to extract IPs from the log file
+    SUSPICIOUS_IPS=($(awk -v one_hour_ago="$ONE_HOUR_AGO" '$1" "$2 >= one_hour_ago && /Failed password/ { for (i=1; i<=NF; i++) { if ($i == "from" && $(i-1) != "Invalid") { print $(i+1) } } }' "$LOG_FILE" | sort | uniq))
 
-    # Only report if there are suspicious IPs to report
-    if [ -s "$TEMP_REPORTED_IPS_FILE" ]; then
-        local JSON_DATA='{"ip_list":['
-        local FIRST_ENTRY=true
-
-        while read -r IP; do
+    # Check for duplicates and add IPs to the temporary list
+    for IP in "${SUSPICIOUS_IPS[@]}"; do
+        if ! [[ " ${TEMP_IP_LIST[*]} " =~ " $IP " ]]; then
             # Check if IP is not in reported_ips file or expired
             IP_EXPIRATION=$(grep "^$IP " "$REPORTED_IPS_FILE" | cut -d ' ' -f 2)
-            CURRENT_TIMESTAMP=$(date +%s)
             if [ -z "$IP_EXPIRATION" ] || [ "$CURRENT_TIMESTAMP" -gt "$IP_EXPIRATION" ]; then
-                if [ "$FIRST_ENTRY" = true ]; then
-                    FIRST_ENTRY=false
-                else
-                    JSON_DATA+=','
-                fi
-                JSON_DATA+="{\"ip\":\"$IP\",\"category\":\"1\",\"info\":\"Failed login attempts detected\",\"port\":\"$SSH_PORT\"}"
+                TEMP_IP_LIST+=("$IP")
 
                 # Add IP and expiration time to reported_ips file (4 hours from now)
                 EXPIRATION_TIME=$((CURRENT_TIMESTAMP + 4 * 3600))  # 4 hours in seconds
                 echo "$IP $EXPIRATION_TIME" >> "$REPORTED_IPS_FILE"
             fi
-        done < "$TEMP_REPORTED_IPS_FILE"
+        fi
+    done
+
+    # Only report if there are suspicious IPs to report
+    if [ ${#TEMP_IP_LIST[@]} -gt 0 ]; then
+        local JSON_DATA='{"ip_list":['
+        local FIRST_ENTRY=true
+
+        for IP in "${TEMP_IP_LIST[@]}"; do
+            if [ "$FIRST_ENTRY" = true ]; then
+                FIRST_ENTRY=false
+            else
+                JSON_DATA+=','
+            fi
+            JSON_DATA+="{\"ip\":\"$IP\",\"category\":\"1\",\"info\":\"Failed login attempts detected\",\"port\":\"$SSH_PORT\"}"
+        done
 
         JSON_DATA+=']}'
 
         # Report all suspicious IPs in a single API request
         report_ip_list "$JSON_DATA"
     fi
-
-    # Clean up temporary file
-    rm "$TEMP_REPORTED_IPS_FILE"
 }
 
-# Example: Extract, validate, and report IPs from the last hour of the SSH log
+# Example: Extract and report IPs from the last hour of the SSH log
 extract_ips_from_ssh_log "$SSH_LOG_FILE"
