c3c1f479aa09fec3c8165efb92917cd93e3d5350
kallemarc <46725076+kallemarc@users.noreply.github.com>
2023-08-12 00:35:03 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -16,56 +16,54 @@ report_ip_list() {
 # Extract IPs from the last hour of the SSH log file and create JSON data
 extract_ips_from_ssh_log() {
     local LOG_FILE="$1"
-    local SUSPICIOUS_IPS=()
+    local TEMP_REPORTED_IPS_FILE="/tmp/reported_ips.txt"
     local TEMP_IP_LIST=()
 
-    # Calculate the timestamp for one hour ago (both formats)
-    ONE_HOUR_AGO_1=$(date -d '1 hour ago' +'%Y-%m-%dT%H:%M:%S')
-    ONE_HOUR_AGO_2=$(date -d '1 hour ago' +'%b %d %H:%M:%S')
+    # Calculate the timestamp for one hour ago
+    ONE_HOUR_AGO=$(date -d '1 hour ago' +'%b %d %H:%M:%S')
 
-    # Use awk to extract IPs from the log file (both formats)
-    SUSPICIOUS_IPS=($(awk -v one_hour_ago_1="$ONE_HOUR_AGO_1" -v one_hour_ago_2="$ONE_HOUR_AGO_2" \
-        '$1" "$2 >= one_hour_ago_1 || $1" "$2 >= one_hour_ago_2 && /Failed password/ { for (i=1; i<=NF; i++) { if ($i == "from" && $(i-1) != "Invalid") { match($(i+1), /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($(i+1), RSTART, RLENGTH); } } }' \
-        "$LOG_FILE" | sort | uniq))
+    # Use awk to extract suspicious entries from the log file and extract IPs
+    awk -v one_hour_ago="$ONE_HOUR_AGO" '
+        $1" "$2 >= one_hour_ago && (/Failed password/ || /Failed password for invalid user/) {
+            match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/, ip)
+            if (ip[0] != "") {
+                print ip[0]
+            }
+        }
+    ' "$LOG_FILE" | sort | uniq > "$TEMP_REPORTED_IPS_FILE"
 
-    # Current timestamp
-    CURRENT_TIMESTAMP=$(date +%s)
+    # Only report if there are suspicious IPs to report
+    if [ -s "$TEMP_REPORTED_IPS_FILE" ]; then
+        local JSON_DATA='{"ip_list":['
+        local FIRST_ENTRY=true
 
-    # Check for duplicates and add IPs to the temporary list
-    for IP in "${SUSPICIOUS_IPS[@]}"; do
-        if ! [[ " ${TEMP_IP_LIST[*]} " =~ " $IP " ]]; then
+        while read -r IP; do
             # Check if IP is not in reported_ips file or expired
             IP_EXPIRATION=$(grep "^$IP " "$REPORTED_IPS_FILE" | cut -d ' ' -f 2)
+            CURRENT_TIMESTAMP=$(date +%s)
             if [ -z "$IP_EXPIRATION" ] || [ "$CURRENT_TIMESTAMP" -gt "$IP_EXPIRATION" ]; then
-                TEMP_IP_LIST+=("$IP")
+                if [ "$FIRST_ENTRY" = true ]; then
+                    FIRST_ENTRY=false
+                else
+                    JSON_DATA+=','
+                fi
+                JSON_DATA+="{\"ip\":\"$IP\",\"category\":\"1\",\"info\":\"Failed login attempts detected\",\"port\":\"$SSH_PORT\"}"
 
                 # Add IP and expiration time to reported_ips file (4 hours from now)
                 EXPIRATION_TIME=$((CURRENT_TIMESTAMP + 4 * 3600))  # 4 hours in seconds
                 echo "$IP $EXPIRATION_TIME" >> "$REPORTED_IPS_FILE"
             fi
-        fi
-    done
-
-    # Only report if there are suspicious IPs to report
-    if [ ${#TEMP_IP_LIST[@]} -gt 0 ]; then
-        local JSON_DATA='{"ip_list":['
-        local FIRST_ENTRY=true
-
-        for IP in "${TEMP_IP_LIST[@]}"; do
-            if [ "$FIRST_ENTRY" = true ]; then
-                FIRST_ENTRY=false
-            else
-                JSON_DATA+=','
-            fi
-            JSON_DATA+="{\"ip\":\"$IP\",\"category\":\"1\",\"info\":\"Failed login attempts detected\",\"port\":\"$SSH_PORT\"}"
-        done
+        done < "$TEMP_REPORTED_IPS_FILE"
 
         JSON_DATA+=']}'
 
         # Report all suspicious IPs in a single API request
         report_ip_list "$JSON_DATA"
     fi
+
+    # Clean up temporary file
+    rm "$TEMP_REPORTED_IPS_FILE"
 }
 
-# Example: Extract and report IPs from the last hour of the SSH log
+# Example: Extract, validate, and report IPs from the last hour of the SSH log
 extract_ips_from_ssh_log "$SSH_LOG_FILE"
